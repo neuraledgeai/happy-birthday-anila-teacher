680c2093914a119539ab74ece38bb90977e0aad0
Anoop Kumar U <anooprockstar10@gmail.com>
2025-05-25 13:12:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -196,3 +196,14 @@ document.head.appendChild(style);
 """
 html(floating_hearts)
 
+html("""
+<script>
+confetti({
+    particleCount: 100,
+    spread: 100,
+    origin: { y: 0.6 }
+});
+</script>
+""")
+
+
